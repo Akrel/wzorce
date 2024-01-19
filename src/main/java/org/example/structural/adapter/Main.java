627c8d57325fa3e48c0bb7146b5f1e2af67d25db
wojtek <wojtekgrelewicz@gmail.com>
2024-01-19 20:38:58 +0100

    OPISY

--- src/main/java/org/example/structural/adapter/Main.java
+++ src/main/java/org/example/structural/adapter/Main.java
@@ -1,5 +1,14 @@
 package org.example.structural.adapter;
 
+/***
+ * Wzorzec Adaptera (ang. Adapter Pattern) to strukturalny wzorzec projektowy, który umożliwia współpracę dwóch klas o różnych interfejsach poprzez dostosowanie
+ * interfejsu jednej z nich do interfejsu drugiej. Jest to szczególnie użyteczne, gdy chcemy użyć istniejącej klasy, ale jej interfejs nie spełnia wymagań naszego systemu.
+ *
+ * Struktura wzorca Adaptera:
+ * 1. Cel (Target): To jest interfejs, który klient chce używać. Wzorzec Adaptera umożliwia dostosowanie obiektu do tego interfejsu.
+ * 2. Adaptowany obiekt (Adaptee): To jest istniejący obiekt, którego interfejs nie spełnia wymagań klienta.
+ * 3. Adapter: Jest to klasa, która implementuje interfejs celu i jednocześnie zawiera obiekt adaptowany. Adapter przekształca wywołania metod interfejsu celu na wywołania metod adaptowanego obiektu.
+ */
 public class Main {
     public static void main(String[] args) {
         KlasaKwadrat klasaKwadrat = new KlasaKwadrat();
